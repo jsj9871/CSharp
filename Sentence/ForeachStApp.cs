b204c09b441a491252089379670712a418b87f90
jsj9871 <75598682+jsj9871@users.noreply.github.com>
2021-03-29 10:28:48 +0900

    Add files via upload

---
+++ Sentence/ForeachStApp.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace sentence
+{
+    class  ForeachStApp
+
+    {
+        static void Main(string[] args)
+        {
+
+
+            string[] color = { "red", "green", "blue" };
+            foreach (string s in color)
+                Console.WriteLine(s);
+
+
+
+
+        }
+    }
+}
+
---
+++ Sentence/IfElseStApp.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace sentence
+{
+    class  IfElseStApp
+    {
+        static void Main(string[] args)
+        {
+
+            int n;
+            Console.Write("Enter a number = ");
+            n = Console.Read() - '0';
+            if (n % 2 == 0)
+                Console.WriteLine(n + " is an even number.");
+            else
+                Console.WriteLine(n + " is an odd number.");
+
+
+        }
+    }
+}
