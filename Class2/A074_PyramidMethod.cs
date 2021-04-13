e0a0d7cf08baf9afb4105615acfd6d4dff784158
jsj9871 <75598682+jsj9871@users.noreply.github.com>
2021-04-13 15:11:26 +0900

    Add files via upload

---
+++ Class2/A074_PyramidMethod.cs
@@ -0,0 +1,29 @@
+using System;
+
+namespace Class2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DrawPyramid(3);
+            DrawPyramid(5);
+            DrawPyramid(7);
+        }
+        static void DrawPyramid(int n)
+        {
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = i; j < n; j++)
+                    Console.Write(" ");
+                for (int k = 1; k <= 2 * i - 1; k++)
+                    Console.Write("*");
+                Console.WriteLine();
+            }
+        }
+    }
+}
+
+
+
+
